38bdf0eab4cecf23edd549e0102b36d8b057158c
Geoffroy Mounier <geoffroymounier@gmail.com>
2018-03-07 15:54:25 +0100

    no message

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,12 +3,11 @@ import Features from '../views/features';
 import Prices from '../views/prices';
 import LocalCurrency from '../views/localcurrency';
 import Company from '../views/company';
-<<<<<<< HEAD
-import ContactForm from '../views/contactform';
-=======
-import ContactForm from '../views/contactform'
 
->>>>>>> a9adf5665efba87c28c6ea6282ec5c2bfb85617c
+import ContactForm from '../views/contactform';
+
+
+
 const routes = [
     { path: '/',
       exact : true,
@@ -25,11 +24,11 @@ const routes = [
   },{ path: '/monkeymoney',
       exact : true,
       main: Company,
-<<<<<<< HEAD
+
   },{ path: '/contactez_nous',
-=======
+
   },{ path: '/contactez-nous',
->>>>>>> a9adf5665efba87c28c6ea6282ec5c2bfb85617c
+
       exact : true,
       main: ContactForm,
   }
